1fda1f83fb916f58ad0d3456299675dcd3090c51
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-01-13 10:54:07 +0800

    Create exercise2.java

---
+++ exercise2.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+       
+        test("Driving");
+        scan.close();
+    }
+
+    static void test(String drive) {
+
+        System.out.println("Driving Test!");
+        driving(drive);
+
+        String feedback = getFeedBack(drive);
+
+        // Define the base case / stopping condition
+        if (feedback.equals("no")) {
+
+            // Do something to reach the goal
+            reTrain(drive);
+
+            // Recursive call
+            test(drive);
+        } else if (feedback.equals("yes")) {
+            gtg(drive);
+        }
+    }
+
+    static void driving(String drive) {
+        System.out.println("Driving School");
+    }
+
+    static String getFeedBack(String drive) {
+        System.out.println("Do i pass the " + drive + " training? (Enter \"yes\" or \"no\")");
+
+        String feedback = scan.next();
+
+        return feedback;
+    }
+
+    static void reTrain(String drive) {
+        System.out.print("what do you lack in training: ");
+        String need = scan.next();
+        System.out.println("You need to: " + need + " more");
+    }
+
+    static void gtg(String drive) {
+        System.out.println("You passed the " + drive + " test");
+
+
+    }
+}
