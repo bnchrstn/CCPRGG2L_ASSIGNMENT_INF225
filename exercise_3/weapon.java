c365d17f3cb02f22123f847ffb75d47437880b9a
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-01-21 18:46:33 +0800

    Update weapon.java

--- exercise_3/weapon.java
+++ exercise_3/weapon.java
@@ -10,23 +10,8 @@ public class Weapon {
         this.damage = newDamage;
     }
 
-    public void sayMyName2() {
-        System.out.println("My sword is a " + name);
-
-    }
-
-    public void sayMyDamage() {
-        System.out.println("its damage is " + damage);
-
-    }
-
-    public void sayMyRarity() {
-        System.out.println("its rarity is " + rarity);
-
-    }
-
     public String showWeaponNameAndRarity() {
-        return this.name + " " + this.rarity;
+        return this.name + " : " + this.rarity;
     }
 
 }
