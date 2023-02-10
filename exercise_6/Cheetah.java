befa0ab8eedcf368002e12258a28e6541a1a51b6
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-02-10 11:40:22 +0800

    Create Cheetah.java

---
+++ exercise_6/Cheetah.java
@@ -0,0 +1,11 @@
+public class Cheetah implements Animal{
+
+    public void walk() {
+        System.out.println("The Cheetah is walking");
+    }
+
+    public void run() {
+        System.out.println("The Cheetah is running");
+    }
+    
+}
