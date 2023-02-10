b60f8665a39bae0023140c92e55b528fbbe1e865
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-02-10 10:07:00 +0800

    Create Daddy.java

---
+++ exercise_5/Daddy.java
@@ -0,0 +1,9 @@
+public class Daddy {
+
+    protected static String surname = "Aceveda";
+
+    public void showLove () {
+        System.out.println("I love my " + surname + " family");
+    }
+
+}
