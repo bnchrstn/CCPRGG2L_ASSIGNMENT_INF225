71f21054ff732952369eb408d70bd8c01fd4f392
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-02-10 11:40:57 +0800

    Create Pet.java

---
+++ exercise_6/Pet.java
@@ -0,0 +1,11 @@
+abstract class Pet {
+
+    // abstract method 
+    public abstract void eat();
+
+    // regular method 
+    public void sleep () {
+        System.out.println("ZZzzzzz");
+    }
+    
+}
