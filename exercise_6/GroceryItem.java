23f4e218e8950c37572b0e5aea3869c589ec9184
Ben Christian Aceveda <101437225+bnchrstn@users.noreply.github.com>
2023-02-10 11:41:41 +0800

    Create GroceryItem.java

---
+++ exercise_6/GroceryItem.java
@@ -0,0 +1,14 @@
+public class GroceryItem {
+    
+    String name; 
+    double price; 
+
+    public void showItemName() {
+        System.out.println(this.name);
+    }
+
+    public void showPrice() {
+        System.out.println(this.price);
+     }
+    
+}
